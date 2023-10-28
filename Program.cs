da8f735e29ca0b168ff8ea65d2348e5e423dfc29
ErDem37 <149205216+ErDem37@users.noreply.github.com>
2023-10-29 00:15:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Lütfen yazılı puanını giriniz");
+                string yazili = Console.ReadLine();
+                Console.WriteLine("Lütfen dinleme puanını giriniz");
+                string dinleme = Console.ReadLine();
+                Console.WriteLine("Lütfen konuşma puanını giriniz");
+                string konuşma = Console.ReadLine();
+                yazilinot not = new yazilinot();
+                bool x = yazilinot.Yazilinot1(yazili, dinleme, konuşma);
+                if (x==false)
+                {
+                    continue;
+                }
+                int konusma = Convert.ToInt32(konuşma);
+                int dinleme1 = Convert.ToInt32(dinleme);
+                int yazılı = Convert.ToInt32(yazili);
+                if (yazılı > 100)
+                {
+                    Console.WriteLine("Yazılı değeri 100'den büyük olamaz.");
+                    continue;
+                }
+                if (konusma > 100)
+                {
+                    Console.WriteLine("Dinleme puanı 100'den büyük olamaz");
+                    continue;
+                }
+                if (dinleme1 > 100)
+                {
+                    Console.WriteLine("Dinleme puanı 100'den büyük olamaz");
+                    continue;
+                }
+                int yazili1 = ((yazılı * 50) / 100);
+                int dinleme2 = ((dinleme1 * 25) / 100);
+                int konusma1 = ((konusma * 25) / 100);
+                Console.WriteLine("Sınav notu=" + (yazili1 + dinleme2 + konusma1));
+                Console.WriteLine("******************************");
+            }
+        }
+    }
+}
---
+++ yazilinot.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class yazilinot
+    {
+        public static bool Yazilinot1(string yazili, string konusma, string dinleme)
+        {
+            for (int i = 0; i < yazili.Length; i++)
+            {
+                char[] yazili1 = yazili.ToCharArray();
+                bool kontrol = char.IsNumber(yazili1[i]);
+                if (kontrol == false)
+                {
+                    Console.WriteLine("Yazılı puanı sadece rakamlardan oluşabilir");
+                    return false;
+                }
+                char[] konusma1 = konusma.ToCharArray();
+                bool kontrol1 = char.IsNumber(konusma1[i]);
+                if (kontrol1 == false)
+                {
+                    Console.WriteLine("Konuşma puanı sadece rakamlardan oluşabilir");
+                    return false;
+                }
+                char[] dinleme1 = dinleme.ToCharArray();
+                bool kontrol2 = char.IsNumber(dinleme1[i]);
+                if (kontrol2 == false)
+                {
+                    Console.WriteLine("Dinleme puanı sadece rakamlardan oluşabilir");
+                    return false;
+
+                }
+            }
+            return true;
+
+        }
+
+    }
+}
